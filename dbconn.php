34987925fe418df767f74a6c67289eb4ff1b3f72
emoie-lk <72979524+emoie-lk@users.noreply.github.com>
2020-10-17 00:58:44 +0530

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,4 +1,4 @@
 <?php
-     $conn = mysqli_connect("un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "vzbc86ufonfw1hgn", "lusu6iqjbs6kl3xt", "gr85gutpdeiz2uaf") 
-     or die("Could not connect to the database  osshhaaddhhaa");
+     $conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "b1421f9040c5ca", "c6a54f55", "heroku_ee69a9c396c762c") 
+     or die("Could not connect to the database");
 ?>
