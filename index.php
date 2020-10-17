58178ef5fe1d95be1d450237faa6678becad5232
emoie-lk <72979524+emoie-lk@users.noreply.github.com>
2020-10-17 14:40:55 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,67 @@ require_once('dbconn.php');
 
 ?>
 
+
+
+<div id="carouselExampleIndicators" class="col-lg-11 carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/imghome.png" width="auto" height="300px" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/imghome1.png" width="auto" height="300px" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/imghome2.png" width="auto" height="300px" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/imghome3.png" width="auto" height="300px" alt="Fourth slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   <?php
   $sql = "SELECT `item_code`, `item_name`, `discount_price` FROM tbl_items";
   $stmt = mysqli_prepare($conn, $sql);
