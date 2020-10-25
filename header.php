86395918ec27c30266a0593e2a1013a929592a90
emoie-lk <72979524+emoie-lk@users.noreply.github.com>
2020-10-25 14:10:55 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -79,9 +79,9 @@ require_once('dbconn.php');
 
 <div id="search" class="container fixed ">
   <div class="row justify-content-center">
-    <div class="col-sm col-md col-lg col-xl">
+    <div class="col-3 col-md-3 col-lg col-xl-3">
 
-      <a href="index.php"> <img  class="item-img" width="208px" height="57px" alt="N/A" src="images/logo.png"> </a>
+      <a href="index.php"> <img  class="item-img" width="90%" height="auto" alt="N/A" src="images/logo.png"> </a>
 
     </div>
   <!--  <div class="col-sm-6">
@@ -91,22 +91,25 @@ require_once('dbconn.php');
       </form>        
     </div> -->
 
-    <div class="col-sm- col-md- col-lg- col-xl-6">
+    <div class="col-6 col-md-6 col-lg- col-xl-6">
       <form class="form-inline my-2 my-lg-0" method="get" action="index.php">
         <input name="searchbar" id = "textfield" class="form-control mr-sm-2 searchtext" type="search" placeholder="Search">
         <button id = "btn" class="btn btn-outline-success my-2 my-sm-0 searchbtn" type="submit">Search</button>
       </form>
     </div>
 
-    <div class="col-sm col-md col-lg col-xl mn">
+    <div class="col-1 col-md-1 col-lg col-xl-1 cart">
+
+      <a class="cart" href="cart.php"><img  class="item-img" width="50%" height="auto" alt="N/A" src="images/cart.png"></a>
+
+    </div>
+
+    <div class="col-2 col-md-2 col-lg col-xl-2 seclogo">
   
-
-      <a class="cart" href="cart.php"><svg width="40" height="40" viewBox="0 0 24 24"><path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm9.804-16.5l-3.431 12h-2.102l2.542-9h-5.993c.113.482.18.983.18 1.5 0 3.59-2.91 6.5-6.5 6.5-.407 0-.805-.042-1.191-.114l1.306 3.114h13.239l3.474-12h1.929l.743-2h-4.196zm-6.304 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm-4.5-10.5c0 2.485-2.018 4.5-4.5 4.5-2.484 0-4.5-2.015-4.5-4.5s2.016-4.5 4.5-4.5c2.482 0 4.5 2.015 4.5 4.5zm-2-.5h-2v-2h-1v2h-2v1h2v2h1v-2h2v-1z"/></svg> </a>
-      
-      <img class="securelogo" class="item-img" width="145px" height="65px" alt="N/A" src="images/payment.png">
-
+      <img class="securelogo" class="item-img" width="80%" height="auto" alt="N/A" src="images/payment.png">
     
     </div>
+
   </div>
 </div>
 
